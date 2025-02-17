15d24b8702685722f175c7b70e21ef464d81ec2b
Amol Gosavi <87413926+amol-1811@users.noreply.github.com>
2025-02-17 15:42:34 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Unit_Testing;
+
+public class Program
+{
+    public int Addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Subtraction(int  a, int b)
+    {
+        return a - b;
+    }
+    public static void Main(string[] args)
+    {
+        //Program p = new Program();
+
+        //p.Addition(1, 2);
+        //p.Subtraction(1, 2);
+
+        //int sum = p.Addition(1, 2);
+        //Console.WriteLine(sum);
+
+        //int sub = p.Subtraction(1, 2);
+        //Console.WriteLine(sub);
+        PrimeNum.CheckPrime();
+    }
+}
